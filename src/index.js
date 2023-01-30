f7b8811e266dcea2164e18c4739d3bcb180cd4c7
Angelo Velandia <angelo.velandiar@grupomok.com>
2023-01-30 10:30:40 -0500

    :alien: Ajustes de exportacion de funciones de forma temporal

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
-import { formatCpf } from "./fns/cpf";
-export { formatCpf }
+import { formatCpf, formatCpfChange, clearFormatCpf, isValidCpf } from "./fns/cpf";
+import { formatPhone, formatPhoneChange, clearFormatPhone, isValidPhone } from "./fns/phone";
+export { formatCpf, formatCpfChange, clearFormatCpf, isValidCpf, formatPhone, formatPhoneChange, clearFormatPhone, isValidPhone }
